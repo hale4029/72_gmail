2295c6a0e7956844a4a9cd9c76f1cc474f0e6267
Harrison Levin <harrison483@gmail.com>
2020-01-23 14:51:32 -0700

    refactor, gitignore

--- quickstart/lib/store_dir.py
+++ quickstart/lib/store_dir.py
@@ -1,9 +1,5 @@
-#from quickstart import main
 from message import GetMessage
 import numpy as np
-
-#service = main()
-#msg_id = '16fd3813f0ad7990'
 
 def FolderName(service, msg_id):
     '''previously created labels in GMAIL'''
@@ -17,11 +13,7 @@ def FolderName(service, msg_id):
     try:
         message_labels = message['labelIds']
         label_id = list(filter(lambda x: 'Label' in x, message_labels)).pop()
-        #label_id= message['labelIds'][1]
-        #import pdb; pdb.set_trace()
         folder_name = list(filter(lambda label: label['id'] == label_id, labels)).pop()
         return folder_name['name']
     except:
          return '_unassigned'
-
-#FolderName(service, msg_id)
